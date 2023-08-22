5d17b24b45f3d3d9f21e7eb8d86f53a036596e3a
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2023-08-23 00:15:15 +0100

    Intial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt />
+    <TheFooter/>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/layouts/TheHeader.vue'
+import TheFooter from '@/components/layouts/TheFooter.vue'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+}
+</script>
